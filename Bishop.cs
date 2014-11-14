d26162e17f5b42c5b5d8dd26b5331d51c169ffe5
DanielLopezWin14 <daniel.lopez.win14@utb.hermods.se>
2014-11-14 10:01:20 +0100

    försöker få rätt på detta nu

---
+++ Bishop.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewProjectChess
+{
+    class Bishop :ChessPiece
+    {
+        public static List<List<Position>> Moves = new List<List<Position>>();
+
+        public Bishop(int x, int y, string color)
+        {
+            GetPositionX = x;
+            GetPositionY = y;
+            this.color = color;
+                                
+            Moves.Add(new List<Position>
+            {
+                new Position(1, 1),
+                new Position(2, 2),
+                new Position(3, 3),
+                new Position(4, 4),
+                new Position(5, 5),
+                new Position(6, 6),
+                new Position(7, 7)
+            });
+
+                                 
+            Moves.Add(new List<Position>
+            {
+                new Position(1, -1),
+                new Position(2, -2),
+                new Position(3, -3),
+                new Position(4, -4),
+                new Position(5, -5),
+                new Position(6, -6),
+                new Position(7, -7)
+            });
+                                    
+            Moves.Add(new List<Position>
+            {
+                new Position(-1, 1),
+                new Position(-2, 2),
+                new Position(-3, 3),
+                new Position(-4, 4),
+                new Position(-5, 5),
+                new Position(-6, 6),
+                new Position(-7, 7)
+            });
+                                    
+            Moves.Add(new List<Position>
+            {
+                new Position(-1, -1),
+                new Position(-2, -2),
+                new Position(-3, -3),
+                new Position(-4, -4),
+                new Position(-5, -5),
+                new Position(-6, -6),
+                new Position(-7, -7)
+            });
+
+        }
+
+        public override string Describe()
+        {
+            return "I am a Bishop, at postion "
+                   + GetPositionX + ", " + GetPositionY;
+        }
+
+        public override string GetChessType()// kallar på denna när man vill ha ut typen
+        {
+            return "Bishop";
+        }
+
+        public override string GetColor()// när man vill ha ut color värdet på pjäsen kallas denna metod
+        {
+            return color;
+        }
+
+        public override string GetSign()
+        {
+            if(GetColor()== "White")
+                return "wB";
+            else
+            {
+                return "bB";
+            }
+        }
+    }
+}
---
+++ ChessPiece.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewProjectChess
+{
+    public class ChessPiece
+    {
+        protected string color;
+        public int GetPositionX { get; set; }
+        public int GetPositionY { get; set; }
+        
+        public virtual string Describe()
+        {
+            return "I am at position " + GetPositionX + ", " + GetPositionY;
+        }
+
+        public virtual string GetChessType()
+        {
+            return "Chesspiece type should be written here.";
+        }
+
+        public virtual string GetColor()
+        {
+            return "Color should be written here.";
+        }
+
+        public virtual string GetSign()
+        {
+            return "chesstype sign";
+        }
+    }
+}
+//public int CurrentPosition
+//{
+//    get
+//    {
+//        throw new System.NotImplementedException();
+//    }
+//    set
+//    {
+//    }
+//}
+//public void ChangePosition()
+//{
+//    // This method will have to get activated after the NewPosition() has calculated the best move "new position".
+//    //And then move the piece, change the Currentposition property to the new position.
+//}
+//public void NewPostion()
+//{
+//    throw new System.NotImplementedException();
+//}
